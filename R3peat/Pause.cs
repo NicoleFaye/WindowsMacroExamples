13db1a688560914ec659cf5eb958065b7e455f28
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-08 13:18:06 -0800

    added Pause

---
+++ R3peat/Pause.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace R3peat
+{
+    class Pause: Action
+    {
+        private int Delay;
+        public override void run() {
+            Thread.Sleep(this.Delay);
+        }
+        public Pause(int milliseconds)
+        {
+            this.Delay= milliseconds;
+        }
+    }
+}
